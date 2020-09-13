25a8d637ba924c5045c68d785e1f2967d01ec7b4
Lajos Koszti <ajnasz@ajnasz.hu>
2020-09-13 19:19:38 +0200

    Set Access control headers only if origin is not empty

--- api.go
+++ api.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"log"
 	"net/http"
 )
 
 func setupResponse(w *http.ResponseWriter, req *http.Request) {
-	(*w).Header().Set("Access-Control-Allow-Origin", req.Header.Get("ORIGIN"))
-	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
-	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding")
+	if req.Header.Get("ORIGIN") != "" {
+		(*w).Header().Set("Access-Control-Allow-Origin", req.Header.Get("ORIGIN"))
+		(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
+		(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding")
+	}
 }
 
 func handleRequest(w http.ResponseWriter, r *http.Request) {
@@ -20,6 +23,7 @@ func handleRequest(w http.ResponseWriter, r *http.Request) {
 		setupResponse(&w, r)
 		if r.URL.Path != "/" && r.URL.Path != "" {
 			http.Error(w, "Not found", http.StatusNotFound)
+			log.Println("Not found", r.URL.Path)
 			return
 		}
 		handleCreateEntry(w, r)
