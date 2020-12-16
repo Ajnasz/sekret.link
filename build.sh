20f7ae6a3fc2a2a8f38f120edc4443c80f8cd08e
Lajos Koszti <ajnasz@ajnasz.hu>
2020-12-16 14:12:02 +0100

    Github action to create release with assets

--- build.sh
+++ build.sh
@@ -1,13 +1,20 @@
 #!/bin/sh
 
+set -eu
+
 build() {
 	VERSION=$(git describe --tags)
 	BUILD=$(date +%FT%T%z)
 	ARCHLIST="$1"
 	OSLIST="$2"
 	STORAGE="$3"
-	echo $VERSION
-	echo $BUILD
+	BUILD_DIR="$4"
+
+	echo "$VERSION"
+	echo "$BUILD"
+
+  mkdir -p $BUILD_DIR
+
 	for os in $OSLIST
 	do
 		for arch in $ARCHLIST
@@ -15,23 +22,24 @@ build() {
 			for storage in $STORAGELIST
 			do
 				echo "building $os.$arch.$storage"
-				GOOS=$os GOARCH=$arch go build -tags $storage -ldflags "-w -s -X main.version=${VERSION} -X main.build=${BUILD}" -o "sekret.link.$os.$arch.$storage"
+				GOOS="$os" GOARCH="$arch" go build -tags "$storage" -ldflags "-w -s -X main.version=${VERSION} -X main.build=${BUILD}" -o "$BUILD_DIR/sekret.link.$os.$arch.$storage"
 			done
 		done
 	done
 }
 
 remove() {
+  BUILD_DIR="$1"
 	for os in $OSLIST
 	do
 		for arch in $ARCHLIST
 		do
 			for storage in $STORAGELIST
 			do
-				if [ -f "sekret.link.$os.$arch.$storage" ]
+				if [ -f "$BUILD_DIR/sekret.link.$os.$arch.$storage" ]
 				then
 					echo "removing $os.$arch.$storage"
-					rm "sekret.link.$os.$arch.$storage"
+					rm "$BUILD_DIR/sekret.link.$os.$arch.$storage"
 				fi
 			done
 		done
@@ -39,10 +47,17 @@ remove() {
 }
 
 REMOVE=0
-OSLIST="linux darwin freebsd"
+OSLIST="linux"
 ARCHLIST="amd64 386"
 STORAGELIST="postgres redis sqlite"
+BUILD_DIR="./build"
+STORAGE=""
 BUILD=0
+
+if [ $# -lt 1 ];then
+  echo "Command required, available commands are \"test\" and \"build\"" >&2
+  exit 1
+fi
 
 subcommand="$1"
 shift
@@ -55,12 +70,12 @@ case "$subcommand" in
 		BUILD=1;
 		;;
 	*)
-		echo "Invalid command, available commands are \"test\" and \"build\""
+		echo "Invalid command, available commands are \"test\" and \"build\"" >&2
 		exit 1
 		;;
 esac
 
-while getopts "tra:o:s:" opt
+while getopts "ra:o:s:b:" opt
 do
 	case "$opt" in
 		"r")
@@ -75,6 +90,9 @@ do
 		"s")
 			STORAGELIST="$OPTARG"
 			;;
+    "b")
+      BUILD_DIR="$OPTARG"
+      ;;
 		[?])
 			exit 1
 			;;
@@ -82,7 +100,7 @@ do
 done
 
 if [ $REMOVE -eq 1 ];then
-	remove
+	remove "$BUILD_DIR"
 else
-	build "$ARCHLIST" "$OSLIST" "$STORAGE"
+	build "$ARCHLIST" "$OSLIST" "$STORAGE" "$BUILD_DIR"
 fi
