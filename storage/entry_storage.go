8038dd9ce86630c5cf9f9c7ad76ced336f44f66f
Lajos Koszti <ajnasz@ajnasz.hu>
2022-01-30 20:15:56 +0100

    split entity storage interface to reader and writer

--- storage/entry_storage.go
+++ storage/entry_storage.go
@@ -6,12 +6,25 @@ import (
 	"github.com/Ajnasz/sekret.link/entries"
 )
 
-type EntryStorage interface {
-	Close() error
-	Create(string, []byte, time.Duration, int) error
-	// Get(string) (*Entry, error)
+type EntryStorageReader interface {
 	GetAndDelete(string) (*entries.Entry, error)
 	GetMeta(string) (*entries.EntryMeta, error)
+	// Closes connection to data storage, like database
+	// Executed on application shutdown
+	Close() error
+}
+
+type EntryStorageWriter interface {
+	// Writes the secret into the remote data storege
+	Create(UUID string, entry []byte, expiration time.Duration, maxReads int) error
 	Delete(string) error
 	DeleteExpired() error
+	// Closes connection to data storage, like database
+	// Executed on application shutdown
+	Close() error
+}
+
+type EntryStorage interface {
+	EntryStorageReader
+	EntryStorageWriter
 }
