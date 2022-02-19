b68d30e350bfbe0c7bf914f1e14a007cac3dfa1f
Lajos Koszti <ajnasz@ajnasz.hu>
2022-02-19 16:47:32 +0100

    add documentation for testhelper

--- testhelper/psql.go
+++ testhelper/psql.go
@@ -6,6 +6,7 @@ import (
 	"github.com/Ajnasz/sekret.link/config"
 )
 
+// GetPSQLTestConn returns connection string for tests
 func GetPSQLTestConn() string {
 	return config.GetConnectionString(fmt.Sprintf("postgres://%s:%s@%s:%d/%s?sslmode=disable", "secret_link_test", "Km61HJgJbBjNA0FdABpjDmQxEz008PHAQMA8TLpUbnlaKN7U8G1bQGHk0wsm", "localhost", 5432, "secret_link_test"))
 }
