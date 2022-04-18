c16152abf6f82895b90be66dca4ed52ecdaee2f6
Lajos Koszti <ajnasz@ajnasz.hu>
2022-04-18 21:10:41 +0200

    dummy_encrypter documentation

--- storage/dummy_encrypter.go
+++ storage/dummy_encrypter.go
@@ -2,14 +2,18 @@ package storage
 
 import "encoding/base64"
 
+// DummyEncrypter encrypter implementation for tests
 type DummyEncrypter struct{}
 
+// Encrypt encrypts data
 func (d *DummyEncrypter) Encrypt(data []byte) ([]byte, error) {
 	output := make([]byte, base64.RawStdEncoding.EncodedLen(len(data)))
 	base64.RawStdEncoding.Encode(output, data)
 
 	return output, nil
 }
+
+// Decrypt denrypts data
 func (d *DummyEncrypter) Decrypt(data []byte) ([]byte, error) {
 	output := make([]byte, base64.RawStdEncoding.DecodedLen(len(data)))
 	_, err := base64.RawStdEncoding.Decode(output, data)
@@ -20,6 +24,7 @@ func (d *DummyEncrypter) Decrypt(data []byte) ([]byte, error) {
 	return output, nil
 }
 
+// NewDummyEncrypter creates a new DummyEncrypter instance
 func NewDummyEncrypter() *DummyEncrypter {
 	return &DummyEncrypter{}
 }
