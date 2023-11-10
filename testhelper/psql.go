29c6d87167189f199fc0e16b584251ff52db5402
Lajos Koszti <ajnasz@ajnasz.hu>
2023-11-10 10:02:55 +0100

    get test postgres password from env var

--- testhelper/psql.go
+++ testhelper/psql.go
@@ -2,11 +2,17 @@ package testhelper
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/Ajnasz/sekret.link/config"
 )
 
 // GetPSQLTestConn returns connection string for tests
 func GetPSQLTestConn() string {
-	return config.GetConnectionString(fmt.Sprintf("postgres://%s:%s@%s:%d/%s?sslmode=disable", "secret_link_test", "Km61HJgJbBjNA0FdABpjDmQxEz008PHAQMA8TLpUbnlaKN7U8G1bQGHk0wsm", "localhost", 5432, "secret_link_test"))
+	password := os.Getenv("POSTGRES_PASSWORD")
+
+	if password == "" {
+		password = "sekret_link_test"
+	}
+	return config.GetConnectionString(fmt.Sprintf("postgres://%s:%s@%s:%d/%s?sslmode=disable", "postgres", "password", "localhost", 5432, password))
 }
