951dcd41a047429ac347d6730c04268f33a649fa
Lajos Koszti <ajnasz@ajnasz.hu>
2024-03-09 17:43:43 +0100

    remove entries and encrypter packages

--- encrypter/encrypter.go
+++
@@ -1,7 +0,0 @@
-package encrypter
-
-// Encrypter interface to encrypt and decrypt byte arrays
-type Encrypter interface {
-	Encrypt([]byte) ([]byte, error)
-	Decrypt([]byte) ([]byte, error)
-}
--- entries/entry.go
+++
@@ -1,7 +0,0 @@
-package entries
-
-// Entry struct represents an entry with it's data and meta data
-type Entry struct {
-	EntryMeta
-	Data []byte
-}
