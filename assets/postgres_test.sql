3031d60b949ce4e156208a81f0986c2589d78629
Lajos Koszti <ajnasz@ajnasz.hu>
2025-07-03 13:50:25 +0200

    fix: fix test db name

--- assets/postgres_test.sql
+++ assets/postgres_test.sql
@@ -1,2 +1,2 @@
-docker exec -i postgres psql -U postgres -c "CREATE USER secret_link_test WITH PASSWORD 'Km61HJgJbBjNA0FdABpjDmQxEz008PHAQMA8TLpUbnlaKN7U8G1bQGHk0wsm'"
-docker exec -i postgres psql -U postgres -c "CREATE DATABASE secret_link_test WITH ENCODING='UTF-8' OWNER secret_link_test"
+docker exec -i postgres psql -U postgres -c "CREATE USER sekret_link_test WITH PASSWORD 'Km61HJgJbBjNA0FdABpjDmQxEz008PHAQMA8TLpUbnlaKN7U8G1bQGHk0wsm'"
+docker exec -i postgres psql -U postgres -c "CREATE DATABASE sekret_link_test WITH ENCODING='UTF-8' OWNER sekret_link_test"
